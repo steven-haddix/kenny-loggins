87952d93f4880ee0e871b2cb6c35f4fa86c9bfba
Haddix, Steven <Steven.Haddix@wendys.com>
2016-05-10 15:16:31 -0400

    Added the ability to use different console function in console appender. Added console appender tests.

--- src/appenders/console.js
+++ src/appenders/console.js
@@ -1,7 +1,5 @@
-/**
- * Created by shadd01 on 4/12/16.
- */
 import BaseAppender from './BaseAppender';
+import format from './layouts/simple';
 import { defaults, injectConfig } from './config/defaults';
 
 export default class ConsoleAppender extends BaseAppender {
@@ -13,7 +11,38 @@ export default class ConsoleAppender extends BaseAppender {
 	}
 
 	doAppend(loggingEvent) {
-		// TODO: layout should be configurable
-		console.log(this.layout(loggingEvent));
+		if (!loggingEvent || !loggingEvent.message || !loggingEvent.level) {
+			return false;
+		}
+
+		const level = loggingEvent.level.toString();
+
+		// TODO: implement configurable layouts
+		const message = format(level, loggingEvent.message);
+
+		return ConsoleAppender.console(level, message);
 	}
+
+	/* eslint-disable no-console */
+	static console(level, message) {
+		switch (level) {
+			case 'FATAL':
+			case 'ERROR':
+				console.error(message);
+				return true;
+			case 'WARN':
+				console.warn(message);
+				return true;
+			case 'DEBUG':
+			case 'INFO':
+				console.info(message);
+				return true;
+			case 'TRACE':
+				console.trace(message);
+				return true;
+			default:
+				return false;
+		}
+	}
+	/* eslint-enable no-console */
 }
---
+++ test/appenders/Console.spec.js
@@ -0,0 +1,32 @@
+import ConsoleAppender from '../../src/appenders/Console';
+import expect, { spyOn } from 'expect'
+
+describe('Appenders', () => {
+    describe('ConsoleAppender', () => {
+        let consoleAppender, consoleSpy;
+
+        beforeEach(() => {
+            consoleAppender = new ConsoleAppender();
+            consoleSpy = spyOn(console, 'trace');
+        })
+
+        afterEach(() => {
+            consoleAppender = null;
+            expect.restoreSpies();
+        })
+
+        it('validates logging event parameters', () => {
+            expect(consoleAppender.doAppend(null)).toBe(false);
+        })
+
+        it('handles invalid level', () => {
+            expect(ConsoleAppender.console('INVALID', 'test message')).toBe(false);
+            expect(consoleSpy).toNotHaveBeenCalled();
+        })
+
+        it('calls correct console function', () => {
+            ConsoleAppender.console('TRACE', 'test message');
+            expect(consoleSpy).toHaveBeenCalledWith('test message');
+        })
+    })
+});
